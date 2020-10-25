77c89b8d6b10eeb5de25da655f6ad31f1bd990c5
Shweta Boddupalli <sboddupalli@unomaha.edu>
2020-10-25 15:15:38 -0500

    imported heroku in settings.py

--- BloodBank/BloodBank/settings.py
+++ BloodBank/BloodBank/settings.py
@@ -95,8 +95,6 @@ DATABASES = {
     }
 }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -134,7 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'users.UserProfile'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+django_heroku.settings(locals())
